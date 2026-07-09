aa2fc39a162c149424e7b3f2fa8451decf4a7e33
JanadaSroor <jokarasheks@gmail.com>
2026-07-10 01:55:25 +0300

    Extension runner: custom message handler for qDebug capture
    - Install QtMessageHandler to capture qDebug output to OutputPanel
    - Try multiple activation hooks: init_ext, init, main, open_panel
    - QApplication::processEvents() after execution for GUI popups

--- viora_ide/core/extension_runner.cpp
+++ viora_ide/core/extension_runner.cpp
@@ -1,19 +1,39 @@
-/*
- * Copyright 2026 Janada Sroor
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include "extension_runner.h"
 #include <QRegularExpression>
+#include <QFile>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QApplication>
+#include <QDebug>
 #include "../../core/flux/engine/flux_script_engine.h"
 
 namespace IDE {
 
-ExtensionRunner::ExtensionRunner(QObject* parent)
-    : QObject(parent) {
+// Custom message handler to capture qDebug output for the OutputPanel
+static ExtensionRunner* s_runnerInstance = nullptr;
+static QtMessageHandler s_originalHandler = nullptr;
+
+static void ideMessageHandler(QtMsgType type, const QMessageLogContext& context, const QString& msg) {
+    // Forward to original handler
+    if (s_originalHandler) s_originalHandler(type, context, msg);
+
+    // Capture debug/warning messages to the OutputPanel
+    if (s_runnerInstance && (type == QtDebugMsg || type == QtWarningMsg)) {
+        QMetaObject::invokeMethod(s_runnerInstance, "outputReceived", Qt::QueuedConnection,
+            Q_ARG(QString, msg));
+    }
 }
 
-bool ExtensionRunner::runSource(const QString& source) {
+ExtensionRunner::ExtensionRunner(QObject* parent)
+    : QObject(parent) {
+    // Install custom message handler to capture qDebug output
+    if (!s_runnerInstance) {
+        s_originalHandler = qInstallMessageHandler(ideMessageHandler);
+        s_runnerInstance = this;
+    }
+}
+
+bool ExtensionRunner::runSource(const QString& source, const QString& extensionDir) {
 #ifdef HAVE_FLUXSCRIPT
     if (m_running) return false;
 
@@ -34,8 +54,40 @@ bool ExtensionRunner::runSource(const QString& source) {
 
     if (success) {
         outputReceived("Script compiled successfully.");
-        eng.callFunction("init", {});
-        eng.callFunction("open_panel", {});
+
+        // Parse manifest.json to see if a custom activate hook is defined
+        QString onActivateHook = "init";
+        bool hasCustomHook = false;
+
+        if (!extensionDir.isEmpty()) {
+            QString manifestPath = extensionDir + "/manifest.json";
+            QFile file(manifestPath);
+            if (file.open(QIODevice::ReadOnly)) {
+                QJsonDocument doc = QJsonDocument::fromJson(file.readAll());
+                if (doc.isObject()) {
+                    QJsonObject obj = doc.object();
+                    if (obj.contains("hooks") && obj["hooks"].isObject()) {
+                        QJsonObject hooks = obj["hooks"].toObject();
+                        if (hooks.contains("onActivate") && hooks["onActivate"].isString()) {
+                            onActivateHook = hooks["onActivate"].toString();
+                            hasCustomHook = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (hasCustomHook) {
+            eng.callFunction(onActivateHook.toUtf8().constData(), {});
+        } else {
+            // Try common activation function names
+            eng.callFunction("init_ext", {});
+            eng.callFunction("init", {});
+            eng.callFunction("main", {});
+            eng.callFunction("open_panel", {});
+        }
+        // Process GUI events so popups (flux_qt_msg_box) can display
+        QApplication::processEvents();
         outputReceived("Extension activated.");
     } else {
         parseAndHighlightErrors(error);
