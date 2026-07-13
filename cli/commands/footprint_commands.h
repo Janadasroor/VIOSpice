a21788495d2464136e236fbbe82e6f000aad3c97
JanadaSroor <jokarasheks@gmail.com>
2026-07-14 02:23:50 +0300

    feat: extend viora cli to support footprint rendering via footprint-render command

---
+++ cli/commands/footprint_commands.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2026 Janada Sroor
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef FOOTPRINT_COMMANDS_H
+#define FOOTPRINT_COMMANDS_H
+
+void registerFootprintCommands();
+
+#endif // FOOTPRINT_COMMANDS_H
