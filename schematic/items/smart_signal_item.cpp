0939c35d4aac3b6aabfdc1967144c2927cce8ec5
JanadaSroor <jokarasheks@gmail.com>
2026-07-19 14:17:48 +0300

    Exclude SmartSignalItem from PCB layout by default

--- schematic/items/smart_signal_item.cpp
+++ schematic/items/smart_signal_item.cpp
@@ -12,6 +12,7 @@
 SmartSignalItem::SmartSignalItem(QPointF pos, QGraphicsItem* parent)
     : SchematicItem(parent)
     , m_size(80, 60) {
+    setExcludeFromPcb(true);
     setPos(pos);
     setFlags(QGraphicsItem::ItemIsSelectable |
              QGraphicsItem::ItemIsMovable |
